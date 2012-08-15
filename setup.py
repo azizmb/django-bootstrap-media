ebabd25ce426d05ca5a9502c6517f1f5c5876223
Aziz M. Bookwala <aziz.mansur@gmail.com>
2012-08-15 16:42:12 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='django-bootstrap-media',
+    version='0.2.0.4',
+    url='https://github.com/azizmb/django-bootstrap-media',
+    author='Aziz M. Bookwala',
+    author_email='aziz.mansur@gmail.com',
+    license='Apache License 2.0',
+    packages=['bootstrap_media'],
+    include_package_data=True,
+    description="Static files from Twitter's Bootstrap",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
